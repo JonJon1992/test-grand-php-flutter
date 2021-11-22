2ae8f77dcb901edbbd2894ac797dd9a77effa1cd
Jonathan Neves <jonathan201392127@gmail.com>
2021-11-22 12:54:23 -0300

    heroku

--- env.php
+++ env.php
@@ -1,6 +1,6 @@
 <?php
 putenv('MYSQL_HOST=database-2.cllvp4sj9oji.us-east-1.rds.amazonaws.com');
-putenv('MYSQL_USER=root');
+putenv('MYSQL_USER=admin');
 putenv('MYSQL_PASS=yiwe3qZuQxLR7iR0bBsu');
 putenv('MYSQL_DB=test_php');
 putenv('MYSQL_PORT=3306');
